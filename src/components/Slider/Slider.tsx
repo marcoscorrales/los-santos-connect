db5ed0bceb7e00f592bb913aa2dd9477de051bba
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-09-27 19:14:14 +0200

    I add react-icons and change slider styles

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import styles from "./Slider.module.scss";
 import sliderData from "../../data/data";
+import { FaPlus } from "react-icons/fa";
 
 export const Slider = () => {
 	return (
 		<div className={styles.slider}>
 			<div className={styles.slider__container}>
 				{sliderData.map((item, index) => (
-					<p className={styles.slider__item} key={index}>{item}</p>
+					<p className={styles.slider__item} key={index}>
+						<FaPlus color="#1D8348" size={20} />
+						{item}
+					</p>
 				))}
 			</div>
 			<div className={styles.slider__container}>
 				{sliderData.map((item, index) => (
-					<p className={styles.slider__item} key={index}>{item}</p>
+					<p className={styles.slider__item} key={index}>
+						<FaPlus color="#1D8348" size={20} />
+						{item}
+					</p>
 				))}
 			</div>
 		</div>
